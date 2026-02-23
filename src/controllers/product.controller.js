09021ef6f2c18e33f108e76c051bf2509d528c94
irving0604 <irving06@uw.edu>
2026-02-22 18:23:54 -0800

    Update product.controller.js

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,40 +1,44 @@
 const db = require("../config/database");
 
-const {Parser} = require('json2csv');
+const { Parser } = require("json2csv");
 const json2csvParser = new Parser();
 
-
-// /**
-//  * getRecords: Obtains all records
-//  */
-// getRecords: Obtains all records
-exports.getRecord = async(req, res) => {//
-    const response = await db.query('SELECT * FROM "tblrecord" ORDER BY id ASC');//
+/**
+ * getRecords: Obtains all records
+ */
+exports.getRecord = async (req, res) => {
+    const response = await db.query('SELECT * FROM "tblrecord" ORDER BY id ASC');
     const json = JSON.stringify(response);
-    res.status(200).send(json);//
-};//
+    res.status(200).send(json);
+};
 
+/**
+ * addRecord: Inserts user data into tblrecord
+ */
+exports.addRecord = async (req, res) => {
+    let { contributor, content, shop_name, favorite_order, lat, lng } = req.body;
 
+    console.log(
+        'INSERT INTO "tblrecord"(contributor, content, shop_name, favorite_order, lat, lng) VALUES ($1, $2, $3, $4, $5, $6)',
+        [contributor, content, shop_name, favorite_order, lat, lng]
+    );
 
-// /**
-//  * Insert Comment/Review: Inserts user insert data of review into tblReview in the database
-//  * @param {form} req - form body that contains user selected information
-//  * @param {status} res - confirmation that comment has been added into the review table
-//  */
-exports.addRecord = async(req, res) => {
-    let {contributor, content, lat, lng} = req.body;
-    // let currTime = new Date().toISOString();
-    console.log('INSERT INTO "tblrecord"(contributor, content, lat, lng) VALUES ($1, $2, $3, $4)',
-        [contributor, content, lat, lng]);
-    let {recordRows} = await db.query(
-        'INSERT INTO "tblrecord"(contributor, content, lat, lng) VALUES ($1, $2, $3, $4)',
-        [contributor, content, lat, lng]
-    )
+    let { recordRows } = await db.query(
+        'INSERT INTO "tblrecord"(contributor, content, shop_name, favorite_order, lat, lng) VALUES ($1, $2, $3, $4, $5, $6)',
+        [contributor, content, shop_name, favorite_order, lat, lng]
+    );
 
     res.status(200).send({
         message: "record added into record table!",
         body: {
-            record: {contributor, content, lat, lng}
+            record: {
+                contributor,
+                content,
+                shop_name,
+                favorite_order,
+                lat,
+                lng
+            }
         }
-    })
+    });
 };
